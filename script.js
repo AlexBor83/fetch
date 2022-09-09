d553cb47147cb0d699319151f9d865b79f3cd209
AlexBor83 <rabota_for_alex@mail.ru>
2022-09-09 20:06:47 +0400

    отправка данных XMLHttpRequest

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
 console.log('hello');
 
+// отправка данных
+// const sendData = (userData) => {
+//   fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     body: JSON.stringify(userData),
+//     headers: {
+//       'Content-type': 'application/json; charset=UTF-8',
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+//     .catch((error) => console.log(error));
+// };
 
 // отправка данных
 const sendData = (userData) => {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      body: JSON.stringify(userData),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch(error => console.log(error))
+  const xhr = new XMLHttpRequest();
+  xhr.open('post', 'https://jsonplaceholder.typicode.com/posts', true);
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+  xhr.send(JSON.stringify(userData));
+
+  xhr.onload = function () {
+    if (xhr.status === 404) {
+      alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+    } else {
+      alert(`Готово, получили ${xhr.response.length} байт`);
+    }
   };
+
+  xhr.onerror = function () {
+    alert('Запрос не получен');
+  };
+};
 
 // получение данных
 const getData = () => {
   fetch('./db.json')
     .then((response) => response.json())
     .then((data) => {
-    //    console.log(data)
-    //   return data;
-    sendData(data)
+      //    console.log(data)
+      //   return data;
+      sendData(data);
     })
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 };
 
 getData();
-
